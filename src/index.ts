b6505d8fa907bbd214e7a2013d32f10d21518961
MichaelBreitung <mibreit@gmx.de>
2024-07-26 08:28:42 +0000

    missing exports added

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import './index.css';
 export { createLazyLoaderFromElements, createLazyLoader } from './factories/createLazyLoader';
 export { ELazyMode } from './types';
 export type { default as ILazyLoader } from './interfaces/ILazyLoader';
+export type { default as IElementInfo } from './interfaces/IElementInfo';
 export { default as LazyLoader } from './components/LazyLoader';
 export { default as ScrollLoader } from './components/ScrollLoader';
 export { default as Element } from './components/Element';
