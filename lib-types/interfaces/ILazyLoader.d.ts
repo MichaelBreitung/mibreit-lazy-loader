55f0f46086f6b891a7b0463fb65c3173b93d7884
Michael Breitung <43609079+MichaelBreitung@users.noreply.github.com>
2024-07-29 12:40:47 +0200

    medium: sequential async loading (#4)

--- lib-types/interfaces/ILazyLoader.d.ts
+++ lib-types/interfaces/ILazyLoader.d.ts
@@ -3,8 +3,8 @@
  * @copyright Michael Breitung Photography (www.mibreit-photo.com)
  */
 export default interface ILazyLoader {
-    loadAll(): void;
-    setCurrentIndex(newIndex: number): void;
+    loadAll(): Promise<void>;
+    setCurrentIndex(newIndex: number): Promise<void>;
     loadElement(index: number): Promise<boolean>;
     getUnloadedElementIndices(): Array<number>;
 }
